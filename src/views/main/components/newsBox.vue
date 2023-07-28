8fb9540816d8d76137ae84cc1f321754cd8d7277
BlackGarland <1626479137@qq.com>
2023-07-28 21:11:08 +0800

    提交

---
+++ src/views/main/components/newsBox.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="newsBox">
+    <div v-if="type==1" class="newsBox_top1">
+      <img :src="list[0].img" alt="">
+      <div class="newsBox-right">
+        <p class="newsBox-right_title">{{list[0].title}}</p>
+        <p class="newsBox-right_des">{{list[0].des}} <span>[详细]</span></p>
+      </div>
+    </div>
+    <div v-if="type==3" class="newsBox_top1">
+      <div class="newsBox-coverImg">
+        <span>新华时评：1233112313123123123123</span>
+      </div>
+    </div>
+    <div v-if="type==4" class="newsBox_top2">
+      <div class="newsBox_top2-header">
+        <div class="newsBox_top2-header-left">
+          <img class="newsBox_top2-header-icon"
+            src="https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg" alt="">
+          <p>媒体上的96711</p>
+        </div>
+        <div class="newsBox_top2-header-more">更多 ></div>
+      </div>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="报刊" name="bk"></el-tab-pane>
+        <el-tab-pane label="配置管理" name="sp"></el-tab-pane>
+        <el-tab-pane label="新媒体" name="xmt"></el-tab-pane>
+      </el-tabs>
+      <div class="newsBox_top2-imgBox">
+        <img src="https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg" alt="">
+        <img src="https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg" alt="">
+      </div>
+
+    </div>
+    <ul class="newsBox-list" v-if="type !==4">
+      <li v-for="(item,i) in list.slice(Number(topLength), list.length)" :key="i">
+        <span>{{item.title}}</span>
+      </li>
+    </ul>
+    <ul class="newsBox-list" v-else>
+      <li v-for="(item,i) in listData.slice(Number(topLength), list.length)" :key="i">
+        <span>{{item.title}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SvgIcon',
+    props: {
+      list: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      },
+      type: {
+        type: Number,
+        default: 0
+      },
+
+    },
+    watch: {
+      // type(oldval, newval) {
+
+
+      // }
+    },
+    created() {
+      switch (this.type) {
+        case 1, 3:
+          this.topLength = 1;
+          break
+        case 2, 5:
+          this.topLength = 0;
+          break
+        case 4:
+          this.topLength = 2;
+
+      }
+    },
+    data() {
+      return {
+        listData: this.list,
+        activeName: 'bk'
+      }
+    },
+    methods: {
+      handleClick(val) {
+        // this.
+      }
+    },
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .newsBox {
+    &_top1 {
+      display: flex;
+      margin-bottom: 20px;
+
+      img {
+        width: 150px;
+        height: 100px;
+        margin-right: 20px;
+      }
+    }
+
+    &_top2 {
+      &-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 40px;
+        line-height: 40px;
+        align-items: center;
+
+        &-icon {
+          width: 20px;
+          height: 20px;
+          margin-right: 10px;
+        }
+
+        p {
+          font-size: 16px;
+          font-weight: 600;
+          color: blue;
+
+        }
+
+        &-left {
+          display: flex;
+          align-items: center;
+        }
+
+        &-more {
+          font-size: 16px;
+          color: blue;
+        }
+
+
+      }
+
+      &-imgBox {
+        display: flex;
+        height: 80px;
+        justify-content: space-between;
+
+        img {
+          width: 47%;
+        }
+      }
+
+      ::v-deep .el-tabs__nav-wrap::after {
+        display: none !important;
+      }
+
+      ::v-deep.el-tabs__header {
+        margin-bottom: 0px !important;
+      }
+
+
+      ::v-deep .el-tabs__active-bar {
+        display: none;
+      }
+    }
+
+    &-right {
+      &_title {
+        font-size: 20px;
+        line-height: 24px;
+        font-weight: 500;
+        //   text-align: center;
+        word-break: break-word;
+        margin-bottom: 2px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+
+      &_des {
+        font-size: 14px;
+        line-height: 18px;
+        text-indent: 28px;
+        color: rgb(179, 179, 179);
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+
+        span {
+          color: rgba(0, 153, 255, 0.801);
+        }
+      }
+    }
+
+    &-list {
+      list-style-type: square;
+
+      li {
+
+        list-style-position: outside;
+        // line-height: 20px;
+        // border-radius: 50%;
+        // color: gray;
+        font-size: 16px;
+
+        span {
+          text-overflow: ellipsis;
+          overflow: hidden;
+          word-break: break-all;
+          white-space: nowrap;
+        }
+
+      }
+
+      // line-height: 14px;
+      li::before {
+        //使用伪类插入符号
+        content: "●";
+        font-size: 20px;
+        color: gray;
+        padding-right: 8px;
+      }
+
+    }
+
+    &-coverImg {
+      flex: 1;
+      height: 130px;
+      background-image: url('https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg');
+      background-size: 100% 130px;
+      background-repeat: no-repeat;
+      display: flex;
+      flex-direction: column;
+
+      span {
+        display: block;
+        margin-top: auto;
+        font-size: 16px;
+        line-height: 25px;
+        height: 25px;
+        background-color: rgba(0, 0, 0, 0.3);
+      }
+    }
+
+
+  }
+
+</style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -9,12 +9,20 @@
       </div>
       <div class="main-top_box">
         <el-carousel trigger="click" height="100px">
-          <el-carousel-item v-for="item in top_carouselList" :key="item">
+          <el-carousel-item v-for="(item,i) in top_carouselList" :key="i">
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
         <div class="main-top_box-pic">
           <img src="https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg" alt="">
+        </div>
+      </div>
+      <div class="main-header">
+        <div class="main-header-title">中央军委举行上将极盛一时</div>
+        <div class="main-header-bottom">
+          <div v-for="(item,i) in headerBottomList" :key="i"><span class="main-header-bottom-item"
+              @click="toDetail(item.id)">{{item.title}}</span><span v-if="i!==headerBottomList.length-1">|</span>
+          </div>
         </div>
       </div>
       <div class="main-carousel">
@@ -23,8 +31,68 @@
             <img class="main-carousel-img" :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+        <swiper />
       </div>
-      <swiper />
+      <div class="main-middle">
+        <div class="main-middle-msg">
+          <el-tabs v-model="activeName_1" @tab-click="handleClick">
+            <el-tab-pane label="统帅足印" name="tszy">
+              <!-- type: 子组件判断样式类型用-->
+              <newsBox :list="middle.listTszy" :type="1"></newsBox>
+
+            </el-tab-pane>
+            <el-tab-pane label="重要论述" name="zyls">重要论述</el-tab-pane>
+            <el-tab-pane label="评论解读" name="pljd">评论解读</el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg">
+          <el-tabs v-model="activeName_2" @tab-click="handleClick">
+            <el-tab-pane label="军政要闻" name="jzyw">
+              <newsBox :list="middle.listTszy" :type="2"></newsBox>
+
+            </el-tab-pane>
+            <el-tab-pane label="社论评论" name="slpl">社论评论</el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg">
+          <el-tabs v-model="activeName_3" @tab-click="handleClick">
+            <el-tab-pane label="社论评论" name="slpl">
+              <newsBox :list="middle.listTszy" :type="3"></newsBox>
+
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg">
+          <el-tabs v-model="activeName_3" @tab-click="handleClick">
+            <el-tab-pane label="社论评论" name="slpl">
+              <!-- type: 子组件判断样式类型用-->
+              <newsBox :list="middle.listTszy" :type="4"></newsBox>
+
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg">
+          <el-tabs v-model="activeName_4" @tab-click="handleClick">
+            <el-tab-pane label="军政要闻" name="zwjd">
+              <newsBox :list="middle.listTszy" :type="2"></newsBox>
+
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg"></div>
+        <div class="main-middle-msg"></div>
+        <div class="main-middle-msg">
+          <span class="main-middle-msg-title">网上读书</span>
+          <el-tabs v-model="activeName_8" @tab-click="handleClick">
+            <el-tab-pane label="利剑阅读" name="ljyd">
+              <!-- type: 子组件判断样式类型用-->
+              <newsBox :list="middle.listTszy" :type="1"></newsBox>
+
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="main-middle-msg"></div>
+      </div>
     </div>
 
   </div>
@@ -33,13 +101,29 @@
 <script>
   import "./css/index.scss";
   import swiper from './components/swiper'
+  import newsBox from './components/newsBox'
   export default {
     name: 'MainPage',
     components: {
-      swiper
+      swiper,
+      newsBox
     },
     data() {
       return {
+        // 大标题底部5个按钮的数据
+        headerBottomList: [{
+            title: '中国共产党',
+            id: '123123'
+          },
+          {
+            title: '中国共产党',
+            id: '123123'
+          },
+          {
+            title: '中国共产党',
+            id: '123123'
+          }
+        ],
         //12个导航栏
         navList: [{
             imgUrl: 'https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg',
@@ -105,10 +189,53 @@
           {
             imgUrl: 'https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg'
           }
-        ]
+        ],
+        middle: {
+          listTszy: [{
+              title: '112313123123000',
+              id: '001',
+              des: '描述描述描述描述描述描述描述描述描述描述',
+              img: 'https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg'
+            },
+            {
+              title: '112313123123',
+              id: '001',
+              des: '描述描述描述描述描述描述描述描述描述描述',
+              img: 'https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg'
+            },
+            {
+              title: '112313123123',
+              id: '001',
+              des: '描述描述描述描述描述描述描述描述描述描述',
+              img: 'https://img.zcool.cn/community/01193959eeec64a801202b0c23804b.jpg@1280w_1l_2o_100sh.jpg'
+            }
+          ]
+        },
+        // tab切换相关
+        activeName_1: 'tszy',
+        activeName_2: 'jzyw',
+        activeName_3: 'slpl',
+        activeName_4: 'zwjd',
+        activeName_5: 'gggs',
+        activeName_8: 'ljyd'
       };
     },
-    methods: {},
+    created() {
+      //初始化数据
+      this.initial()
+
+    },
+    methods: {
+      // 新闻跳转详情
+      toDetail(id) {
+        this.$router.push({
+          name: 'newsDetail',
+          params: {
+            id: id
+          }
+        })
+      }
+    },
   };
 
 </script>
